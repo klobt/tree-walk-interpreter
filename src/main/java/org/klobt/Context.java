f3f90d2ae163752f78072a2e5552d7d232ab882c
klobt <111685189+klobt@users.noreply.github.com>
2024-11-30 18:15:37 +0100

    Remove unnecessary things
    This is no longer planned to be a lua interpreter.

--- src/main/java/org/klobt/Context.java
+++ src/main/java/org/klobt/Context.java
@@ -1,21 +1,13 @@
 package org.klobt;
 
-import org.klobt.table.TableStore;
-
 public class Context {
     private final String input;
-    private final TableStore tableStore;
 
     public Context(String input) {
         this.input = input;
-        tableStore = new TableStore();
     }
 
     public String getInput() {
         return input;
-    }
-
-    public TableStore getTableStore() {
-        return tableStore;
     }
 }
